7bd091cc83ff43299a708ca99365f0b1e110cdac
rizkyghidayat <rizkyghidayat@gmail.com>
2015-06-06 01:58:04 +0700

    Update kebersihan_Kelas.java

--- src/tugasbesar/kebersihan_Kelas.java
+++ src/tugasbesar/kebersihan_Kelas.java
@@ -8,28 +8,57 @@ import java.util.Scanner;
 
 /**
  *
- * @author AHO
+ * @author XOC
  */
 abstract public class kebersihan_Kelas {
-    String sirkulasi;
-    int cahaya;
-    int kelembaban;
-    int suhu;
+    private String sirkulasi;
+    private int cahaya;
+    private int kelembaban;
+    private int suhu;
     
     Scanner keb=new Scanner(System.in);
-    
-    public void mlebu(String hallo){
-        System.out.print("Keadaan sirkulasi udara = ");
-        sirkulasi=keb.next();
-        System.out.print("Nilai pencahayaan       = ");
-        cahaya=keb.nextInt();
-        System.out.print("Prosentase kelembaban   = ");
-        kelembaban=keb.nextInt();
-        System.out.print("Suhu (celcius)          = ");
-        suhu=keb.nextInt();
+
+    public kebersihan_Kelas(String sirkulasi, int cahaya, int kelembaban, int suhu) {
+        this.sirkulasi = sirkulasi;
+        this.cahaya = cahaya;
+        this.kelembaban = kelembaban;
+        this.suhu = suhu;
     }
+
     
-    String analisisSirkulasi(){
+    public String getSirkulasi() {
         return sirkulasi;
     }
+
+    public void setSirkulasi(String sirkulasi) {
+        this.sirkulasi = sirkulasi;
+    }
+
+    public int getCahaya() {
+        return cahaya;
+    }
+
+    public void setCahaya(int cahaya) {
+        this.cahaya = cahaya;
+    }
+
+    public int getKelembaban() {
+        return kelembaban;
+    }
+
+    public void setKelembaban(int kelembaban) {
+        this.kelembaban = kelembaban;
+    }
+
+    public int getSuhu() {
+        return suhu;
+    }
+
+    public void setSuhu(int suhu) {
+        this.suhu = suhu;
+    }
+ 
+    public abstract void  mlebu();
+    public abstract void  analisis();
+    public abstract void output();
 }

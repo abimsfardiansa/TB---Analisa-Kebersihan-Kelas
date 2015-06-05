febe9548ad04cb78cec885079cca9920fdeeab92
rizkyghidayat <rizkyghidayat@gmail.com>
2015-06-06 02:00:15 +0700

    Update analisis_kebersihan.java

--- src/tugasbesar/analisis_kebersihan.java
+++ src/tugasbesar/analisis_kebersihan.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -5,55 +8,68 @@
 
 /**
  *
- * @author AHO
+ * @author XOC
  */
+
 public class analisis_kebersihan extends kebersihan_Kelas{
-    public analisis_kebersihan(){
-        System.out.println("\n\tAnalisis Kebersihan Ruang Kelas");
-        mlebu("analisis kebersihan");
+    Scanner er=new Scanner(System.in);
+    public analisis_kebersihan(String sirkulasi, int cahaya, int kelembaban, int suhu){
+        super(sirkulasi, cahaya, kelembaban, suhu);
+    }
+    @Override
+    public void mlebu(){
+        System.out.print("Keadaan sirkulasi udara = ");
+        setSirkulasi(keb.next());
+        System.out.print("Nilai pencahayaan       = ");
+        setCahaya(keb.nextInt());
+        System.out.print("Prosentase kelembaban   = ");
+        setKelembaban(keb.nextInt());
+        System.out.print("Suhu (celcius)          = ");
+        setSuhu(keb.nextInt());
     }
     
     @Override
-    public String analisisSirkulasi(){
-        if(sirkulasi.equals("lancar")){
+    public void analisis(){
+       
+        if(getSirkulasi().equals("lancar")){
             System.out.println("\tSirkulasi udara sesuai.");
         }
         else{
             System.out.println("\tSirkulasi udara tidak sesuai.");
         }
-        return sirkulasi;
-    }
-    
-    public int analisisPencahayaan(){
-        if(cahaya>=250 &&cahaya<=350){
-            System.out.println("\tNilai pencahayaan "+cahaya+", sesuai.");
+        
+        
+        if(getCahaya()>=250 &&getCahaya()<=350){
+            System.out.println("\tNilai pencahayaan "+getCahaya()+", sesuai.");
         }
         else{
-            System.out.println("\tNilai pencahayaan "+cahaya+", tidak sesuai.");
+            System.out.println("\tNilai pencahayaan "+getCahaya()+", tidak sesuai.");
         }
-        return cahaya;
-    }
-    
-    public int analisisKelembaban(){
-        if(kelembaban>=70 &&kelembaban<=80){
-            System.out.println("\tKelembaban ruang "+kelembaban+"%, sesuai.");
+        
+        
+        if(getKelembaban()>=70 &&getKelembaban()<=80){
+            System.out.println("\tKelembaban ruang "+getKelembaban()+"%, sesuai.");
         }
         else{
-            System.out.println("\tKelembaban ruang "+kelembaban+"%, tidak sesuai.");
+            System.out.println("\tKelembaban ruang "+getKelembaban()+"%, tidak sesuai.");
         }
-        return kelembaban;
-    }
-    
-    
-    public int analisisSuhu(){
-        if(suhu>=25 &&suhu<=35){
-            System.out.println("\tSuhu "+suhu+" drajat celcius, sesuai.");
+        
+        
+        if(getSuhu()>=25 &&getSuhu()<=35){
+            System.out.println("\tSuhu "+getSuhu()+" drajat celcius, sesuai.");
             System.out.println("------------------------------------------------");
         }
         else{
-            System.out.println("\tSuhu "+suhu+" drajat celcius, tidak sesuai.");
+            System.out.println("\tSuhu "+getSuhu()+" drajat celcius, tidak sesuai.");
             System.out.println("------------------------------------------------");
         }
-        return suhu;
     }
+    @Override
+    public void output(){
+        System.out.println("sirkulasi udara = "+getSirkulasi());
+        System.out.println("nilai pencahayaan = "+getCahaya());
+        System.out.println("prosentase kelembaban = "+getKelembaban());
+        System.out.println("suhu = "+getSuhu());
+    }
+  
 }

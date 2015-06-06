891ddb5900e7376183a1cc284761ce06a41a4b02
abimsfardiansa <abims@gmx.com>
2015-06-06 10:34:20 +0700

    Update analisis_kondisi1.java

--- src/tugasbesar/analisis_kondisi1.java
+++ src/tugasbesar/analisis_kondisi1.java
@@ -8,7 +8,7 @@
  * @author AHO
  */
 public class analisis_kondisi1 extends kondisi_Kelas{
-    public analisis_kondisi1(){
+  /*  public analisis_kondisi1(){
         System.out.println("\n\tAnalisa kondisi kelas");
         masuk("Analisa kondisi kelas");
     }
@@ -67,6 +67,76 @@ public class analisis_kondisi1 extends kondisi_Kelas{
           //  return false;
         }
         return jendela;
+    }*/
+    
+    
+    
+    public analisis_kondisi1(int panjang, int lebar, int luas, int kursi, int pintu, int jendela){
+        super(panjang, lebar, luas, kursi, pintu, jendela);
+        System.out.println("\n\tAnalisa kondisi kelas");
+        masuk();
+    }
+
+    
+    
+    @Override
+    public int Luas(){
+        int luasruang=getPanjang()*getLebar();
+        System.out.println("\tLuas Kelas = "+luasruang+" meter.");
+        if(getPanjang()!=getLebar()){
+            System.out.println("\tRuang kelas sesuai, yaitu persegi panjang.");
+          //  return true;
+        }
+        else{
+            System.out.println("\tRuang kelas tidak sesuai, yaitu persegi.");
+          //  return false;
+        }
+        return getPanjang();
+        }
+    @Override
+    public float rasioluas(){
+        int luasrg=getPanjang()*getLebar();
+        float rasio=luasrg/getKursi();
+        System.out.print("\tRasio luas = "+rasio);
+        if(rasio>=0.5){
+            System.out.println(", rasio sesuai.");
+          //  return true;
+        }
+        else{
+            System.out.println(", rasio tidak sesuai.");
+           // return false;
+        }
+        return rasio;
+        }
+    @Override
+    public int jmlpintu(){
+        if(getPintu()>=2){
+            System.out.println("\tJumlah pintu "+getPintu()+", sesuai.");
+           // return true;
+        }
+        else{
+            System.out.println("\tJumlah pintu "+getPintu()+", tidak sesuai.");
+          //  return false;
+        }
+        return getPintu();
+    }
+    @Override
+    public int jmljendela(){
+        if(getJendela()>=2){
+            System.out.println("\tJumlah jendela "+getJendela()+", sesuai.");
+            System.out.println("------------------------------------------------");
+          //  return true;
+        }
+        else{
+            System.out.println("\tJumlah jendela "+getJendela()+", tidak sesuai.");
+            System.out.println("------------------------------------------------");
+          //  return false;
+        }
+        return getJendela();
     }
     
+    public void masukan(){
+        System.out.println("panjang "+getPanjang()+", dan lebar "+getLebar());
+        System.out.println("jumlah pintu "+getPintu()+", dan jumlah jendela "+getJendela());
+    }
 }

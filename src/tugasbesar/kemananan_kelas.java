be4dbbaeafb9836413b1f919e013d13dddfe8618
abimsfardiansa <abims@gmx.com>
2015-06-06 10:31:20 +0700

    Update kemananan_kelas.java

--- src/tugasbesar/kemananan_kelas.java
+++ src/tugasbesar/kemananan_kelas.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -7,9 +10,38 @@
  *
  * @author AHO
  */
-public interface kemananan_kelas{
-    void kekokohan();
-    void kuncipintu();
-    void kuncijendela();
-    void bahaya();
+abstract public class kemananan_kelas{
+    
+    /*private String kokoh;
+    private String kpintu;
+    private String kjendela;
+    private String bahaya;*/
+    
+    String kokoh;
+    String kpintu;
+    String kjendela;
+    String bahaya;
+    
+    Scanner k=new Scanner(System.in);
+
+    public kemananan_kelas(String kokoh, String kpintu, String kjendela, String bahaya) {
+        this.kokoh = kokoh;
+        this.kpintu = kpintu;
+        this.kjendela = kjendela;
+        this.bahaya = bahaya;
+    }
+
+    public void amana(){
+        System.out.println("Analisis Keamanan");
+      System.out.print("Kekokohan kelas = ");
+      kokoh=k.next();
+      System.out.print("Kunci pintu = ");
+      kpintu=k.next();
+      System.out.print("Kunci jendela = ");
+      kjendela=k.next();
+      System.out.print("Keamanan kelas = ");
+      bahaya=k.next();
+    }
+    
+    public abstract void aman();
 }
